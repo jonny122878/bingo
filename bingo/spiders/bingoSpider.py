50dd291247c40ea625806b3af6ecfce475982ae0
test2 <test1>
2022-12-18 20:49:15 +0800

    bingoSpider

--- bingo/spiders/bingoSpider.py
+++ bingo/spiders/bingoSpider.py
@@ -4,7 +4,13 @@ import scrapy
 class BingospiderSpider(scrapy.Spider):
     name = 'bingoSpider'
     allowed_domains = ['www.taiwanlottery.com.tw']
-    start_urls = ['http://www.taiwanlottery.com.tw/']
+    start_urls = ['https://www.taiwanlottery.com.tw/lotto/bingobingo/drawing.aspx']
 
     def parse(self, response):
+        # peroid = scrapy.Field()
+        # num20 = scrapy.Field()
+
+        item['peroid'] = response.selector.xpath('/html/body/form/div[3]/div[2]/div[3]/table[2]/tbody/tr/td/table/tbody/tr[4]/td[1]').getall()
+        item['num20'] = response.selector.xpath('/html/body/form/div[3]/div[2]/div[3]/table[2]/tbody/tr/td/table/tbody/tr[4]/td[2]').getall()
+        print('')
         pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from crawler.spiders.lottery import LotterySpider
+from scrapy.crawler import CrawlerProcess
+import json
+import pandas as pd
+import time
+
+
+print('')
