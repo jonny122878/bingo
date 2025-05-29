c6cdadaea6e83874f7a6b11ca531d76900f89312
jonny122878 <bayonet79921@gmail.com>
2025-05-29 13:19:06 +0800

    t

--- parse/TestDeferAlgorithm.py
+++ parse/TestDeferAlgorithm.py
@@ -1,16 +1,11 @@
 import unittest
-from pandas import DataFrame
 from DeferAlgorithm import DeferAlgorithm
 
 class TestDeferAlgorithm(unittest.TestCase):
-    def test___init__(self):
+    def Test_LoadData(self):
         algo = DeferAlgorithm()
-        self.assertIsInstance(algo, DeferAlgorithm)
-
-    def test_calcu(self):
-        algo = DeferAlgorithm()
-        result = algo.calcu([])
-        self.assertIsInstance(result, DataFrame)
+        algo.LoadData([])
+        # No assertion needed as method is pass
 
 if __name__ == "__main__":
     unittest.main()
