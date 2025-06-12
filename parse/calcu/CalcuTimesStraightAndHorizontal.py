1142a59ba90256f1b59782dfb9c682859d6ca7de
jonny122878 <bayonet79921@gmail.com>
2025-06-12 11:28:21 +0800

    t

--- parse/calcu/CalcuTimesStraightAndHorizontal.py
+++ parse/calcu/CalcuTimesStraightAndHorizontal.py
@@ -8,8 +8,9 @@ class CalcuTimesStraightAndHorizontal:
     def Calcu(self, dfStraight: DataFrame, dfHorizontal: DataFrame, Horizontal: IBallMark, Straight: IBallMark) -> None:
         # merge DataFrame
         dfMerged = pd.concat([dfStraight, dfHorizontal], axis=1)
-        # 橫向遍歷，每一列產生兩個欄位：Row N, SortedN，總共 6 欄
+        # 橫向遍歷，整併 dfSortedVariant foreach 到主 for 迴圈，產生 KeyN, SortedN, BallsN 共 9 欄
         sorted_dict = {}
+        variant_dict = {}
         for i in range(dfMerged.shape[0]):
             row_np = dfMerged.iloc[i].to_numpy()
             sorted_indices = np.argsort(row_np)
@@ -17,23 +18,21 @@ class CalcuTimesStraightAndHorizontal:
             sorted_values = np.sort(row_np)
             sorted_dict[f'Row {i+1}'] = sorted_keys
             sorted_dict[f'Sorted{i+1}'] = sorted_values
+            # variant: 跳過前5個元素後取8個元素
+            variant_values = sorted_values[5:13]
+            variant_keys = [dfMerged.columns[idx] for idx in range(5, 13)]
+            variant_balls = []
+            for k in variant_keys:
+                balls = Horizontal.markToBalls(str(k))
+                if not balls:
+                    balls = Straight.markToBalls(str(k))
+                variant_balls.append(balls)
+            variant_dict[f'Key{i+1}'] = variant_keys
+            variant_dict[f'Sorted{i+1}'] = variant_values
+            variant_dict[f'Balls{i+1}'] = variant_balls
         dfSorted = pd.DataFrame(sorted_dict)
+        dfSortedVariant = pd.DataFrame(variant_dict)
         first_row_temp = dfMerged.iloc[0].to_numpy()
-        # variant: 跳過前5個元素後取8個元素
-        variant_values = first_row_temp[5:13]
-        variant_keys = dfMerged.columns[5:13]
-        # 產生 Balls 欄位，優先用 Horizontal.markToBalls，若為空則用 Straight.markToBalls
-        variant_balls = []
-        for k in variant_keys:
-            balls = Horizontal.markToBalls(str(k))
-            if not balls:
-                balls = Straight.markToBalls(str(k))
-            variant_balls.append(balls)
-        dfSortedVariant = pd.DataFrame({
-            'Key': variant_keys,
-            'Sorted': variant_values,
-            'Balls': variant_balls
-        })
         # 建立 dictUnique，key 為 Balls 欄位每個元素，value 為出現次數
         dictUnique = {}
         for balls in variant_balls:
